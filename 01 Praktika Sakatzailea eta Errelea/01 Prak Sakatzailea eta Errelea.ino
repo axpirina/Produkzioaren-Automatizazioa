b22aea58b4e9bd16727893258fb428d96bc2b2df
axpirina <axpirina@oteitzalp.com>
2019-06-07 20:02:58 +0200

    Create 01 Prak Sakatzailea eta Errelea.ino

---
+++ 01 Praktika Sakatzailea eta Errelea/01 Prak Sakatzailea eta Errelea.ino
@@ -0,0 +1,35 @@
+/*              KONTROL  MONOEGONKORRA
+ 
+Hurrengo programak pultsagailu baten irakurketa egiten du eta Irteera bat kontrolatzen du era monoegonkorrean   
+   Ondoren Serie Monitorean idazten ditu balioak.
+
+   Modified By Axpi 2017
+   CC. Creatives Commons.
+*/
+
+// Aldagaien deklarazioa      
+
+int botoia= 2;
+int relay= 8;
+int balioa = 0;
+
+// Sarrera-Irteeren Inizializazioa
+
+void setup() 
+  {
+ Serial.begin (9600);
+ pinMode (botoia, INPUT);
+ pinMode (relay, OUTPUT);
+ digitalWrite(relay,HIGH); //Errelea itzali
+  }
+
+// Programa nagusia
+
+void loop() 
+  {
+ balioa = digitalRead(botoia); //Pultsadorea irakurri
+ digitalWrite(relay,!balioa); //Errelea kontrolatu
+ Serial.print ("Pultsagailuaren balioa=  ");
+ Serial.println (balioa);
+ delay (150); 
+}
