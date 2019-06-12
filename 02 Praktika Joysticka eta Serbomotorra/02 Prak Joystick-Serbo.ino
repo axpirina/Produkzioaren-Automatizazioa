e4005647bf199a6e0b576485a5630790c1562c5d
axpirina <axpirina@oteitzalp.com>
2019-06-12 12:10:19 +0200

    Update 02 Prak Joystick-Serbo.ino

--- 02 Praktika Joysticka eta Serbomotorra/02 Prak Joystick-Serbo.ino
+++ 02 Praktika Joysticka eta Serbomotorra/02 Prak Joystick-Serbo.ino
@@ -1,1 +1,32 @@
-aa
+/* Serbomotor bat joystick bitartez kontrolatzeko programa.
+  by Axpi
+  This example code is in the public domain.
+*/
+#include <Servo.h>
+
+int x = 0;
+int angelua = 0;
+
+Servo Horizontala; // Horizontala Serbo objetua sortzen du
+
+int angeluaSerbo = 90;
+
+void setup() {
+  Serial.begin(9600);
+  Horizontala.attach(6); // Serbo horizontala konektatutzen du Arduinoko 6. Pin-era
+}
+
+void loop() {
+  
+  x = analogRead(0);           // Irakurketa 10 bit-etan (0-1023) balio artean
+  x = map(x, 0, 1023, 0, 255); // Irteerarako mappeatu 8 bit-etara (0-255) balio artean
+  angelua = map(x, 0, 255, 0, 180); // angelua kalkulatu (0-180) balio artean
+  
+  Serial.print("Serbo angelua:   ");
+  Serial.println(angelua);
+  Serial.println(" ");
+  
+  Horizontala.write(angelua);
+  
+  delay(0);
+}
