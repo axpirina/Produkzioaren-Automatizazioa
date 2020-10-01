8d860c0a2a7be32e2c2b2e550c842658a1f541a8
axpirina <axpirina@oteitzalp.com>
2020-10-01 23:46:23 +0200

    Create 02 Automatizazioa Monoegonkorra.ino

---
+++ 02 Automatizazioa Errelea/02 Automatizazioa Monoegonkorra.ino
@@ -0,0 +1,34 @@
+/*              KONTROL  MONOEGONKORRA
+ 
+Hurrengo programak pultsagailu baten irakurketa egiten du eta Irteera bat kontrolatzen du era monoegonkorrean   
+   Ondoren Serie Monitorean idazten ditu balioak.
+   Modified By Axpi 2017
+   CC. Creatives Commons.
+*/
+
+// Aldagaien deklarazioa      
+
+int botoia= 2;
+int relay= 13;
+int balioa = 0;
+
+// Sarrera-Irteeren Inizializazioa
+
+void setup() 
+  {
+ Serial.begin (9600);
+ pinMode (botoia, INPUT);
+ pinMode (relay, OUTPUT);
+ digitalWrite(relay,HIGH); //Errelea itzali
+  }
+
+// Programa nagusia
+
+void loop() 
+  {
+ balioa = digitalRead(botoia); //Pultsadorea irakurri
+ digitalWrite(relay,!balioa); //Errelea kontrolatu
+ Serial.print ("Pultsagailuaren balioa=  ");
+ Serial.println (balioa);
+ delay (150); 
+}
